a684313b394c10d7f3da26ae4cab8e0ad18bbd73
Maroosh-Gillani <76667567+Maroosh-Gillani@users.noreply.github.com>
2023-07-05 22:06:37 -0400

    Added button allowing for grid size changes. CSS needs to be fixed to properly scale with new grid size.

--- script.js
+++ script.js
@@ -1,34 +1,75 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     // Setting up the grid
     const container = document.getElementById('container');
-    console.log(container);
 
-    const gridSize = 16;
-    const divsGrid = [];
+    let gridSize = 16; // default grid size
+    let divsGrid = [];
 
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            const div = document.createElement('div');
-            div.classList.add('square');
-            div.classList.add();
-            divsGrid.push(div);
+    function generateGrid() {
+        // Clear existing grid
+        divsGrid.forEach(div => {
+            div.remove();
+        });
+
+        // Regenerate the grid with the new size
+        divsGrid = [];
+
+        for (let i = 0; i < gridSize; i++) {
+            for (let j = 0; j < gridSize; j++) {
+                const div = document.createElement('div');
+                div.classList.add('square');
+                divsGrid.push(div);
+                container.appendChild(div);
+            }
+        }
+
+        setUpEventListeners();
+    }
+
+    // Hover and press mouse to change square colours
+    function setUpEventListeners() {
+        let isMousePressed = false;
+
+        divsGrid.forEach(div => {
+            div.addEventListener('mouseenter', function () {
+                div.addEventListener('mousedown', function () {
+                    isMousePressed = true;
+                });
+
+                div.addEventListener('mouseup', function () {
+                    isMousePressed = false;
+                });
+
+                if (isMousePressed) {
+                    div.style.backgroundColor = "#000000";
+                }
+            });
+        });
+    }
+
+    generateGrid();
+
+    // Change grid size
+    const changeSizeButton = document.querySelector('.changeGridSize');
+    changeSizeButton.addEventListener('click', function () {
+        setGridSize();
+    });
+
+    function setGridSize() {
+        let size = prompt("Enter grid size here:");
+
+        const sizeInt = parseInt(size);
+
+        if (!isNaN(sizeInt) && Number.isInteger(sizeInt) && sizeInt >= 16 && sizeInt <= 100) {
+            gridSize = sizeInt;
+            generateGrid();
+        }
+        else {
+            gridSize = 16;
+            generateGrid();
         }
     }
 
-    divsGrid.forEach(div => {
-        container.appendChild(div);
-    });
-
-    // Hover to change square colours
-    divsGrid.forEach(div => {
-        div.addEventListener('mouseenter', function() {
-            this.classList.add('hovered');
-
-            if (div.classList.contains('hovered')) {
-                div.style.backgroundColor = "#000000";
-            }
-        })
-    });
 
 });
